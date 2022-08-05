dda6a41d3172eba269c2acbebbe2deb4f9e446b1
sabrina <sabrinaabrodi20@gmail.com>
2022-08-05 16:36:44 +0200

    rating

--- script.js
+++ script.js
@@ -42,6 +42,16 @@ function showMovies(movies) {
   });
 }
 
+function getClassByRate(vote) {
+  if (vote >= 8) {
+    return "green";
+  } else if (vote >= 5) {
+    return "orange";
+  } else {
+    return "red";
+  }
+}
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
